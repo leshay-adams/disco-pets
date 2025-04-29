43968bb7f9eb71f3ee323295bd33df2f2eec447c
leshay-adams <108296148+leshay-adams@users.noreply.github.com>
2025-04-29 11:48:02 +0200

    Tailwind / Vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        tailwindcss()
-    ],
-});
+  plugins: [vue()]
+})
