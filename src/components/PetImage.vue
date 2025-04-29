c30c0591a403ed5be0a4dcce8cb7de54a647030c
leshay-adams <108296148+leshay-adams@users.noreply.github.com>
2025-04-29 08:13:47 +0200

    Fetch from cat and dog apis + loading state + watch

---
+++ src/components/PetImage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+      <div v-if="loading">Loading...</div>
+      <img v-else :src="imageUrl" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['animal'],
+    data() {
+      return {
+        imageUrl: '',
+        loading: false
+      }
+    },
+    watch: {
+      animal: {
+        immediate: true,
+        handler(newVal) {
+          if (newVal) {
+            this.fetchImage(newVal);
+          }
+        }
+      }
+    },
+    methods: {
+      async fetchImage(animal) {
+        this.loading = true;
+        try {
+          if (animal === 'cat') {
+            const res = await fetch('https://api.thecatapi.com/v1/images/search');
+            const data = await res.json();
+            this.imageUrl = data[0].url;
+          } else if (animal === 'dog') {
+            const res = await fetch('https://dog.ceo/api/breeds/image/random');
+            const data = await res.json();
+            this.imageUrl = data.message;
+          }
+        } catch (e) {
+          console.error('Error fetching image', e);
+        } finally {
+          this.loading = false;
+        }
+      }
+    }
+  }
+  </script>
+  
