406bb34e4a50f29b1cc9a9dee8d8adcc8b53bf8e
leshay-adams <108296148+leshay-adams@users.noreply.github.com>
2025-04-29 08:17:53 +0200

    Add animal selection component

---
+++ src/components/PetPicker.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="updateChosenPet('cat')">
+      Cat
+    </button>
+    <button @click="updateChosenPet('dog')">
+      Dog
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    chosenPet: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    updateChosenPet(animal) {
+      this.$emit('update:chosenPet', animal)
+    },
+  }
+}
+</script>
