b54cbeab8f0459cefd04c85fac6c7300aefbe169
leshay-adams <108296148+leshay-adams@users.noreply.github.com>
2025-04-29 08:20:14 +0200

    Add home page with localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,15 @@
-<script setup>
-</script>
-
 <template>
-    <div class="flex justify-center">
-        <a href="https://socialplaces.io/" target="_blank">
-            <img alt="SP logo" class="logo h-[150px]" src="/sp-logo.png"/>
-        </a>
-    </div>
-    <h1>Welcome!</h1>
-    <div class="w-2/3 items-center m-auto py-5">
-        <p class="my-2">
-            We are Social Places; a local marketing software agency that provides listings, reputation, bookings and social interactions across multiple platforms and channels.
-        </p>
-        <p class="my-5">
-            All steps are outlined in the project's <code><u>README</u></code>.
-        </p>
-        <p class="my-2">
-            Good luck, and have fun!
-        </p>
-    </div>
+  <div class="z-10">
+    <Disco />
+  </div>
 </template>
 
-<style scoped>
-.logo {
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
+<script>
+import Disco from './views/Disco.vue'
 
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+export default {
+  components: {
+    Disco
+  }
 }
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
---
+++ src/views/Disco.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Pick Your Grooving Buddy:</h2>
+    <div>
+      <PetPicker v-model:chosenPet="selectedAnimal" />
+      <PetImage :animal="selectedAnimal" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PetPicker from '../components/PetPicker.vue'
+import PetImage from '../components/PetImage.vue'
+
+export default {
+  components: { PetPicker, PetImage },
+  data() {
+    return {
+      selectedAnimal: null
+    }
+  },
+  created() {
+    const saved = localStorage.getItem('selectedAnimal')
+    if (saved) {
+      this.selectedAnimal = saved
+    }
+  },
+  watch: {
+    selectedAnimal(newAnimal) {
+      if (newAnimal) {
+        localStorage.setItem('selectedAnimal', newAnimal)
+      }
+    }
+  }
+}
+</script>
