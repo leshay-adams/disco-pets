0a51252df4c36c885e37d3bd1b2c63fc42b349f9
leshay-adams <108296148+leshay-adams@users.noreply.github.com>
2025-04-30 04:29:22 +0200

    Fix localstorage issue

--- src/classes/local-storage.js
+++ src/classes/local-storage.js
@@ -1,9 +1,14 @@
 export default {
-  get(key) {
-    return JSON.parse(localStorage.getItem(key))
-  },
   set(key, value) {
     localStorage.setItem(key, JSON.stringify(value))
+  },
+  get(key) {
+    const item = localStorage.getItem(key)
+    try {
+      return JSON.parse(item)
+    } catch {
+      return item
+    }
   },
   remove(key) {
     localStorage.removeItem(key)
